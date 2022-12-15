828d86ae1da1e339ff51b3370a4946cb7f93a814
Glowman554 <Glowman434@gmail.com>
2022-12-15 09:39:02 +0000

    clock uwu

--- main.cpp
+++ main.cpp
@@ -4,14 +4,35 @@
  */
 
 #include "mbed.h"
-
+#include <lcd.hpp>
 
 DigitalOut led(LED1);
 
 int main() {
+    RCC->APB1ENR |= 0b00010000;
+    TIM6->PSC = 31999;
+    TIM6->ARR = 1000;
+    TIM6->CNT = 0;
+    TIM6->SR = 0;
+    TIM6->CR1 = 1;
 
+    software_lcd lcd;
+    lcd.set_flags(LCD_CURSOR | LCD_CURSOR_BLINK);
+    lcd.clear();
+
+    int s = 0;
     while (true) {
-        ThisThread::sleep_for(200ms);
-        led = !led;
+        while (!TIM6->SR);
+        TIM6->SR = 0;
+        s++;
+
+        int seconds = s % 60;
+        int minutes = (s / 60) % 60;
+        int hours = (s / 60) / 60;
+
+        char out[0xff] = { 0 };
+        sprintf(out, "%2d:%2d:%2d", hours, minutes, seconds);
+        lcd.cursor(0);
+        lcd.puts(out);
     }
 }
