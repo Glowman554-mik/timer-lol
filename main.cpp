4cae788f4c74dc6ebf37b17be4b850228388828a
Glowman554 <55138266+Glowman554@users.noreply.github.com>
2023-01-15 18:27:59 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -9,12 +9,12 @@
 DigitalOut led(LED1);
 
 int main() {
-    RCC->APB1ENR |= 0b00010000;
-    TIM6->PSC = 31999;
-    TIM6->ARR = 1000;
-    TIM6->CNT = 0;
-    TIM6->SR = 0;
-    TIM6->CR1 = 1;
+    RCC->APB1ENR |= 0b00010000; // enable tim6 clock
+    TIM6->PSC = 31999; // set prescaler
+    TIM6->ARR = 1000; // set auto reload
+    TIM6->CNT = 0; // set count
+    TIM6->SR = 0; // set flag
+    TIM6->CR1 = 1; // enable
 
     software_lcd lcd;
     lcd.set_flags(LCD_CURSOR | LCD_CURSOR_BLINK);
